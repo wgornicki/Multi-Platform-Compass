ec0b82293dbd404b28dd10ecd3d9035a9b356cd1
Wiktor Górnicki <wiktorgornicki@gmail.com>
2023-06-12 13:04:06 +0200

    Basic compass functionality implemented

--- Multi-Platform Compass/CompassPage.xaml.cs
+++ Multi-Platform Compass/CompassPage.xaml.cs
@@ -1,10 +1,83 @@
-﻿namespace Multi_Platform_Compass;
+﻿using Microsoft.Maui.Devices.Sensors;
+using System.Diagnostics;
+
+namespace Multi_Platform_Compass;
 
 public partial class CompassPage : ContentPage
 {
+    bool pointerActive = false;
+
 	public CompassPage()
 	{
 		InitializeComponent();
+        ToggleCompass();
+    }
+
+    private void OnPointerEnter(object sender, PointerEventArgs e)
+    {
+        pointerActive = true;   
+        Point? relativeToPosition = RelativePositionFromCenter(needleContentView, e);
+        needleImage.Rotation = CountRotation(relativeToPosition);
+    }
+
+    private void OnPointerMoved(object sender, PointerEventArgs e)
+    {
+        Point? relativeToPosition = RelativePositionFromCenter(needleContentView, e);
+        needleImage.Rotation = CountRotation(relativeToPosition);
+    }
+
+    private void OnPointerExit(object sender, PointerEventArgs e)
+    {
+        pointerActive = false;
+        needleImage.Rotation = 0;
+    }
+
+    private static Point? RelativePositionFromCenter(View layout, PointerEventArgs e)
+    {
+        Point? layoutPos = e.GetPosition(layout);
+        Point? point = new Point?(new Point(layoutPos.Value.X - layout.Width/2, layoutPos.Value.Y - layout.Height/2));
+        return point;
+    }
+
+    private static double CountRotation(Point? relativeToPosition)
+    {
+        double x = relativeToPosition.Value.X;
+        double y = relativeToPosition.Value.Y;
+        double degrees = 90 + (180 / Math.PI) * Math.Atan2(y,x);
+
+        Debug.WriteLine(relativeToPosition.Value.ToString() + " " + degrees.ToString());
+        return degrees;
+    }
+
+    private void ToggleCompass()
+    {
+        if (Compass.Default.IsSupported)
+        {
+            if (!Compass.Default.IsMonitoring)
+            {
+                // Turn on compass
+                Compass.Default.ReadingChanged += Compass_ReadingChanged;
+                Compass.Default.Start(SensorSpeed.UI, applyLowPassFilter: true);
+            }
+            else
+            {
+                // Turn off compass
+                Compass.Default.Stop();
+                Compass.Default.ReadingChanged -= Compass_ReadingChanged;
+            }
+        }
+    }
+
+    private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+    {
+        if (!pointerActive)
+        {
+            needleImage.Rotation = 0 - e.Reading.HeadingMagneticNorth;
+        }
+
+        // Update UI Label with compass state
+        CompassLabel.TextColor = Colors.Green;
+        CompassLabel.Text = $"Compass: {e.Reading}";
     }
 }
 
